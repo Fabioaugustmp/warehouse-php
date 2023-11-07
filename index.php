38efccc82a6fa6ac93bbceac1a3ce17b0ea8f711
Fabio Augusto Marques Paula <48330222+Fabioaugustmp@users.noreply.github.com>
2023-11-07 19:26:28 -0300

    Add files via upload

--- index.php
+++ index.php
@@ -1,20 +1,20 @@
-<?php
+<?php 
 
-require_once(dirname(__FILE__) . "\src\config\DatabaseHandler.php");
+require 'E:\PROJETOS\warehouse-php\src\config\DatabaseHandler.php';
+
+require_once(dirname(__FILE__) . '\src\model\Product.php');
+
+$product = new Product(12,1, "asdaso", 1, "teste", "adsa", "asd", "asd", "as", "asd");
 
 $sql = 'select * from products';
 
 $result = DatabaseHandler::getResultFromQuery($sql);
 
-echo '<br>';
 print_r($result);
-echo '<br>';
-// print_r($result->fetch_all());
-
-echo '<br>';
-echo '<br>';
 
 while($row = $result->fetch_assoc()) {
     print_r($row);
     echo '<br>';
 }
+
+// var_dump($product);
