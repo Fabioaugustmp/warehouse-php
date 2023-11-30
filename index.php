6851965b3b95f03d66e32fd641319a8bf5b6b13d
Fabio Augusto Marques Paula <fabioaugustomarquespaula@gmail.com>
2023-11-30 19:56:54 -0300

    feat: Organizing routes

--- index.php
+++ index.php
@@ -1,20 +1,23 @@
 <?php
 
 require_once(realpath(dirname(__FILE__, 1)) . '/src/config/config.php');
-
 $uri = urldecode(parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH));
 
-if ($uri === '/' || $uri === '' || $uri === '/index.php') {
-    $uri = '/DashBoardController.php';
+$routes = [
+    '/' => '/DashBoardController.php',
+    '/product/create' => '/ProductControllerCreate.php',
+    '/product' => '/ProductController.php',
+    '/notfound' => '/NotFoundController.php'
+    // Adicione mais rotas conforme necessário
+];
+
+// Verifica se a rota existe no array, caso contrário, redireciona para uma página de erro
+if (array_key_exists($uri, $routes)) {
+    require_once(CONTROLLER_PATH . $routes[$uri]);
+} else {
+    // Rota não encontrada, redireciona para uma página de erro 404 ou lida de outra forma
+    header("HTTP/1.0 404 Not Found");
+    require_once(CONTROLLER_PATH . '/NotFoundController.php');
 }
 
-if ($uri === '/product/create') {
-    $uri = '/ProductControllerCreate.php';
-}
-
-if ($uri === '/product') {
-    $uri = '/ProductController.php';
-}
-
-require_once(CONTROLLER_PATH . "{$uri}");
 
---
+++ src/views/notfound.php
@@ -0,0 +1,13 @@
+   <!-- Begin Page Content -->
+   <div class="container-fluid">
+
+       <!-- 404 Error Text -->
+       <div class="text-center">
+           <div class="error mx-auto" data-text="404">404</div>
+           <p class="lead text-gray-800 mb-5">Página Não Encontrada</p>
+           <p class="text-gray-500 mb-0">Humm algum erro ocorreu...</p>
+           <a href="/">&larr; Dashboard</a>
+       </div>
+
+   </div>
+   <!-- /.container-fluid -->
